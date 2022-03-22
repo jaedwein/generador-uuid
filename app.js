d5db69cbd120ef29fb8ca8d90c279c10182b9029
jaedwein <jaedwein@gmail.com>
2022-03-22 20:48:24 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// Módulos requeridos
+const express = require("express");
+const http = require("http");
+const helmet = require("helmet");
+var compression = require("compression");
+require("dotenv").config();
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+app.use(helmet()); // Ayuda a proteger aplicaciones Express
+app.use(compression());
+
+// Servidor HTTP
+const serverHttp = http.createServer(app);
+serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
+
+// Contenido estático
+app.use(express.static("./public"));
+
+console.log(uuidv4());
+
+// API
+app.get("/api/get-uuid", function (req, res) {
+  console.log(uuidv4());
+  res.send(uuidv4());
+});
+
+// 404
+app.get("*", function (req, res) {
+  res.status(404).send("Error 404 - Recurso no encontrado");
+});
